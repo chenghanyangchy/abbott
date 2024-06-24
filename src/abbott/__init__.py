500b11a8a1ae36756b427c2a965d326e71cb6f69
rhornb <rhornbachner@gmail.com>
2024-06-24 16:08:06 +0200

    Initial commit

---
+++ src/abbott/__init__.py
@@ -0,0 +1,3 @@
+"""
+3D Multiplexed Image Analysis Workflows
+"""
---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+import abbott
+import json
+from pathlib import Path
+
+
+PACKAGE = "abbott"
+PACKAGE_DIR = Path(abbott.__file__).parent
+MANIFEST_FILE = PACKAGE_DIR / "__FRACTAL_MANIFEST__.json"
+with MANIFEST_FILE.open("r") as f:
+    MANIFEST = json.load(f)
+    TASK_LIST = MANIFEST["task_list"]
