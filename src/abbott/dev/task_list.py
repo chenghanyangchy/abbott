f4bc0996a1fea169a28a0461f3331bfcd284cf8a
jluethi <joel.luethi@uzh.ch>
2025-01-08 17:42:16 +0100

    Fix tests & add zenodo-based test for registration

--- src/abbott/dev/task_list.py
+++ src/abbott/dev/task_list.py
@@ -4,8 +4,13 @@ from fractal_tasks_core.dev.task_models import ParallelTask
 
 TASK_LIST = [
     ParallelTask(
-        name="Thresholding Label Task",
-        executable="thresholding_label_task.py",
+        name="Compute Registration elastix",
+        executable="fractal_tasks/compute_registration_elastix.py",
+        meta={"cpus_per_task": 4, "mem": 16000},
+    ),
+    ParallelTask(
+        name="Apply Registration elastix",
+        executable="fractal_tasks/apply_registration_elastix.py",
         meta={"cpus_per_task": 1, "mem": 4000},
     ),
 ]
--- tests/test_registration_workflow.py
+++ tests/test_registration_workflow.py
@@ -4,63 +4,62 @@ from pathlib import Path
 import pytest
 from devtools import debug
 
+from abbott.fractal_tasks.apply_registration_elastix import apply_registration_elastix
+from abbott.fractal_tasks.compute_registration_elastix import (
+    compute_registration_elastix,
+)
 from abbott.fractal_tasks.init_registration_hcs import init_registration_hcs
 
 
 @pytest.fixture(scope="function")
-def test_data_dir(tmp_path: Path) -> str:
+def test_data_dir(tmp_path: Path, zenodo_zarr: Path) -> str:
     """
     Copy a test-data folder into a temporary folder.
     """
-    # FIXME: Get the data from zenodo
-    source_dir = (
-        "/Users/joel/Desktop/20241031_abbott/AssayPlate_Greiner_CELLSTAR655090.zarr"
-    )
     dest_dir = (tmp_path / "registration_data").as_posix()
-    debug(source_dir, dest_dir)
-    shutil.copytree(source_dir, dest_dir)
+    debug(zenodo_zarr, dest_dir)
+    shutil.copytree(zenodo_zarr, dest_dir)
     return dest_dir
 
 
 def test_registration_workflow(test_data_dir):
-    # TODO: Make path relative
-    # parameter_files = [
-    #     "/Users/joel/Documents/Code/abbott/tests/data/params_translation_level0.txt"
-    # ]
-    # # Task-specific arguments
-    # wavelength_id = "A01_C01"
-    # roi_table = "FOV_ROI_table"
-    # level = 0
-    # intensity_normalization = False
-
-    zarr_urls = []
-    print(test_data_dir)
+    parameter_files = [
+        str(Path(__file__).parent / "data/params_rigid.txt"),
+        str(Path(__file__).parent / "data/params_affine.txt"),
+        str(Path(__file__).parent / "data/bspline_lvl2.txt"),
+    ]
+    # Task-specific arguments
+    wavelength_id = "A01_C01"
+    roi_table = "FOV_ROI_table"
+    level = 0
+    reference_acquisition = 19
+    zarr_urls = [f"{test_data_dir}/B/02/0", f"{test_data_dir}/B/02/1"]
 
     parallelization_list = init_registration_hcs(
         zarr_urls=zarr_urls,
         zarr_dir="",
-        reference_acquisition=19,
-    )
-
+        reference_acquisition=reference_acquisition,
+    )["parallelization_list"]
     print(parallelization_list)
 
-    # compute_registration_elastix(
-    #     zarr_url=zarr_url,
-    #     wavelength_id=wavelength_id,
-    #     roi_table=roi_table,
-    #     reference_cycle=reference_cycle,
-    #     parameter_files=parameter_files,
-    #     level=level,
-    #     intensity_normalization=intensity_normalization,
-    # )
+    for param in parallelization_list:
+        compute_registration_elastix(
+            zarr_url=param["zarr_url"],
+            init_args=param["init_args"],
+            wavelength_id=wavelength_id,
+            roi_table=roi_table,
+            lower_rescale_quantile=0.0,
+            upper_rescale_quantile=0.99,
+            parameter_files=parameter_files,
+            level=level,
+        )
 
-    # apply_registration_elastix(
-    #     input_paths=input_paths,
-    #     output_path=output_path,
-    #     component=component,
-    #     metadata=metadata,
-    #     roi_table=roi_table,
-    #     reference_cycle=reference_cycle,
-    #     overwrite_input=False,
-    #     registration_folder="transforms",
-    # )
+    # FIXME: Make this run on all zarr_urls once the task supports this
+    apply_registration_elastix(
+        # Fractal parameters
+        zarr_url=zarr_urls[1],
+        # Core parameters
+        roi_table=roi_table,
+        reference_acquisition=reference_acquisition,
+        overwrite_input=False,
+    )
