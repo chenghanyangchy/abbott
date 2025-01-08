1b1335c145b980201c39b0616009754b47a51437
jluethi <joel.luethi@uzh.ch>
2025-01-08 18:00:51 +0100

    Update to newest template version & add task metadata

--- src/abbott/dev/task_list.py
+++ src/abbott/dev/task_list.py
@@ -4,13 +4,21 @@ from fractal_tasks_core.dev.task_models import ParallelTask
 
 TASK_LIST = [
     ParallelTask(
-        name="Compute Registration elastix",
+        name="Compute Registration (elastix)",
         executable="fractal_tasks/compute_registration_elastix.py",
         meta={"cpus_per_task": 4, "mem": 16000},
+        category="Registration",
+        modality="HCS",
+        tags=["Multiplexing"],
+        docs_info="file:docs_info/calculate_registration.md",
     ),
     ParallelTask(
-        name="Apply Registration elastix",
+        name="Apply Registration (elastix)",
         executable="fractal_tasks/apply_registration_elastix.py",
         meta={"cpus_per_task": 1, "mem": 4000},
+        category="Registration",
+        modality="HCS",
+        tags=["Multiplexing"],
+        docs_info="file:docs_info/apply_registration.md",
     ),
 ]
