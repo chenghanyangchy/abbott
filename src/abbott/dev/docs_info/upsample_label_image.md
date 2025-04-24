b29f1346fd0ad6d2469c95127c4e114f7f970f20
rhornb <rhornbachner@gmail.com>
2025-04-24 13:45:40 +0200

    label upsampling task

---
+++ src/abbott/dev/docs_info/upsample_label_image.md
@@ -0,0 +1,7 @@
+### Purpose
+- Upsamples segmented **labels** in 2D or 3D images to the highest image resolution.
+- Useful if segmentation was performed at a lower resolution (e.g. level 1).
+
+### Outputs
+- A new **upsampled label image** with resolution matching those of reference images.
+- Preserves the integer label values from the original segmentation.
---
+++ tests/test_upsample_label_image.py
@@ -0,0 +1,161 @@
+import logging
+import shutil
+from pathlib import Path
+
+import dask.array as da
+import fractal_tasks_core
+import numpy as np
+import pytest
+import zarr
+from devtools import debug
+from fractal_tasks_core.labels import prepare_label_group
+from fractal_tasks_core.ngff import load_NgffImageMeta
+from fractal_tasks_core.pyramids import build_pyramid
+from fractal_tasks_core.utils import rescale_datasets
+
+from abbott.fractal_tasks.init_select_acquisition_round import (
+    init_select_acquisition_round,
+)
+from abbott.fractal_tasks.upsample_label_image import upsample_label_image
+
+__OME_NGFF_VERSION__ = fractal_tasks_core.__OME_NGFF_VERSION__
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="function")
+def test_data_dir_3d(tmp_path: Path, zenodo_zarr: Path) -> str:
+    """
+    Copy a test-data folder into a temporary folder.
+    """
+    dest_dir = (tmp_path / "upsampled_3d").as_posix()
+    debug(zenodo_zarr, dest_dir)
+    shutil.copytree(zenodo_zarr, dest_dir)
+    return dest_dir
+
+
+def test_upsample_label_image_same_resolutions(test_data_dir_3d):
+    zarr_urls = [f"{test_data_dir_3d}/B/03/0", f"{test_data_dir_3d}/B/03/1"]
+    reference_acquisition = 2
+
+    parallelization_list = init_select_acquisition_round(
+        zarr_urls=zarr_urls,
+        zarr_dir="",
+        reference_acquisition=reference_acquisition,
+    )["parallelization_list"]
+    print(parallelization_list)
+
+    for param in parallelization_list:
+        upsample_label_image(
+            zarr_url=param["zarr_url"],
+            label_name="emb_linked",
+            output_label_name="emb_linked_upsampled",
+            input_ROI_table="emb_ROI_table_2_linked",
+            output_ROI_table="emb_ROI_table_2_linked",
+            level=0,
+            overwrite=True,
+        )
+
+
+def test_upsample_label_image_lower_resolution(test_data_dir_3d):
+    zarr_urls = [f"{test_data_dir_3d}/B/03/0", f"{test_data_dir_3d}/B/03/1"]
+    label_name = "emb_linked"
+
+    zarr_url = zarr_urls[0]
+    # Remove level 0 and rename level 1 to 0
+    label_zarr_url = Path(f"{zarr_url}/labels/{label_name}")
+    label_image = da.from_zarr(f"{label_zarr_url}/1").compute()
+
+    # Update zarr group
+    ngff_image_meta = load_NgffImageMeta(str(zarr_url))
+    coarsening_xy = ngff_image_meta.coarsening_xy
+    num_levels = ngff_image_meta.num_levels
+    new_datasets = rescale_datasets(
+        datasets=[ds.model_dump() for ds in ngff_image_meta.datasets],
+        coarsening_xy=coarsening_xy,
+        reference_level=1,
+        remove_channel_axis=True,
+    )
+
+    label_attrs = {
+        "image-label": {
+            "version": __OME_NGFF_VERSION__,
+            "source": {"image": "../../"},
+        },
+        "multiscales": [
+            {
+                "name": label_name,
+                "version": __OME_NGFF_VERSION__,
+                "axes": [
+                    ax.model_dump()
+                    for ax in ngff_image_meta.multiscale.axes
+                    if ax.type != "channel"
+                ],
+                "datasets": new_datasets,
+            }
+        ],
+    }
+
+    # Prepare new output label path
+    image_group = zarr.group(zarr_url)
+    prepare_label_group(
+        image_group,
+        label_name,
+        overwrite=True,
+        label_attrs=label_attrs,
+        logger=logger,
+    )
+
+    label_path_out = f"{zarr_url}/labels/{label_name}/0"
+    store = zarr.storage.FSStore(label_path_out)
+    label_dtype = np.uint32
+
+    # Ensure that all output shapes & chunks are 3D (for 2D data: (1, y, x))
+    # https://github.com/fractal-analytics-platform/fractal-tasks-core/issues/398
+    data_zyx = da.from_zarr(f"{zarr_url}/{1}")[0]
+    shape = data_zyx.shape
+    if len(shape) == 2:
+        shape = (1, *shape)
+    chunks = data_zyx.chunksize
+    if len(chunks) == 2:
+        chunks = (1, *chunks)
+    mask_zarr = zarr.create(
+        shape=shape,
+        chunks=chunks,
+        dtype=label_dtype,
+        store=store,
+        overwrite=False,
+        dimension_separator="/",
+    )
+
+    da.array(label_image).to_zarr(
+        url=mask_zarr,
+        compute=True,
+    )
+
+    # Create pyramid
+    build_pyramid(
+        zarrurl=f"{zarr_url}/labels/{label_name}",
+        overwrite=True,
+        num_levels=num_levels,
+        coarsening_xy=coarsening_xy,
+        aggregation_function=np.max,
+    )
+
+    parallelization_list = init_select_acquisition_round(
+        zarr_urls=zarr_urls,
+        zarr_dir="",
+        reference_acquisition=2,
+    )["parallelization_list"]
+    print(parallelization_list)
+
+    for param in parallelization_list:
+        upsample_label_image(
+            zarr_url=param["zarr_url"],
+            label_name=label_name,
+            output_label_name="emb_linked_upsampled",
+            input_ROI_table="FOV_ROI_table",
+            output_ROI_table="emb_ROI_table_upsampled",
+            level=0,
+            overwrite=True,
+        )
